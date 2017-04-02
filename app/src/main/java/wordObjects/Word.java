c60b510172eb351bd55f32926e404e7d708b9433
ZiqueYuutaka <izferren@gmail.com>
2017-04-02 13:48:19 -0400

    Creating word model

---
+++ app/src/main/java/wordObjects/Word.java
@@ -0,0 +1,50 @@
+package wordObjects;
+
+/**
+ * Created by Zique Yuutaka on 4/2/2017.
+ */
+
+public class Word {
+
+    private String germanWord;
+    private String englishWord;
+    private String type;
+
+    public Word(String germanWord, String englishWord, String type){
+        setType(type);
+        setGermanWord(germanWord);
+        setEnglishWord(englishWord);
+    }
+
+    public String getGermanWord() {
+        return germanWord;
+    }
+
+    public void setGermanWord(String germanWord) {
+        String temp = germanWord.substring(0,3);
+        if(temp.toLowerCase().equals("das") && type.toLowerCase().equals("noun")){
+            this.germanWord = germanWord;
+        }else if(type.toLowerCase().equals("noun")){
+            this.germanWord = "das " + germanWord;
+        }else{
+            this.germanWord = germanWord;
+        }
+    }
+
+    public String getEnglishWord() {
+        return englishWord;
+    }
+
+    public void setEnglishWord(String englishWord) {
+        this.englishWord = englishWord;
+
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
